20e193a9f81e105d445cfd812a8a181bbe0eeb0a
Hafiz Samaae <hafez@hotmail.com>
2023-04-09 10:50:36 +0000

    update

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,8 +1,42 @@
 import { Component } from '@angular/core';
+import { UserService } from './auth/user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {}
+export class AppComponent {
+
+  me = {
+    id: 0,
+    name: "",
+    email: "seb@seb.seb",
+    password: "sebsebseb",
+  }
+  //LOGIN FORMULÄR
+
+  user2 = {
+    id: 0,
+    name: "",
+    email: "",
+    password: "",
+  }
+
+  constructor(private userService: UserService){
+    //userService.loginUser(this.me)
+  }
+
+  login(){
+    this.userService.loginUser(this.me)
+  }
+
+  logout(){
+    this.userService.logoutUser(this.me)
+  }
+
+  register(){
+    this.userService.registerUser()
+  }
+
+}
--- angular/src/app/auth/user.service.ts
+++ angular/src/app/auth/user.service.ts
@@ -1,13 +1,81 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs';
+
+import { User } from '../user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor(private http: HttpClient) { }
+  name: string = '';
+  email: string = '';
+  password: string = '';
+  password_confirmation: string = '';
+
+  configURL = "http://127.0.0.1:8000/api/";
+
+  httpOptions = {
+    headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer '
+    })
+  }
+
+
+  constructor(private http: HttpClient, private router:Router) { }
+
+
+  loginUser(user: User){
+    this.http.post<any>(this.configURL + "login", user, this.httpOptions)
+    .pipe(catchError(this.handleError))
+    .subscribe(res => {
+      console.log(res)
+      localStorage.setItem("token", res.token);
+    })
+  }
+
+  logoutUser(user: User) {
+    localStorage.removeItem("token");
+    console.log("logged out")
+  }
+
+  registerUser() {
+    
+    const data = {
+      email: this.email,
+      password: this.password,
+      name: this.name,
+      password_confirmation: this.password_confirmation,
+    };
+    this.http.post<any>(this.configURL + "register", this.httpOptions)
+    .pipe(catchError(this.handleError))
+    .subscribe(res => {
+      console.log("register success");
+      localStorage.setItem("token", res.token);
+    })
+  }
+
+
+
+
+
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.status === 0) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong.
+      console.error(
+        `Backend returned code ${error.status}, body was: `, error.error);
+    }
+    // Return an observable with a user-facing error message.
+    return throwError(() => new Error('Something bad happened; please try again later.'));
+  }
 }
